42facb2b6bffa0bcf2dd894cf173d147ac2eb1ed
Anderson Carlos <andersoncarlos28@gmail.com>
2024-08-10 16:41:41 -0400

    feat: add bootstrap theme on body and html

--- src/utils/configure-body.ts
+++ src/utils/configure-body.ts
@@ -4,5 +4,8 @@ export default function configureBody() {
   removeAllChildNodes(document.body);
   document.body.style.overflow = "auto";
   document.body.classList.add("bg-dark");
-  document.querySelector("html")!.style.fontSize = "unset";
+  const html = document.querySelector("html")!;
+  html.style.fontSize = "unset";
+  html.dataset.bsTheme = "dark";
+  document.body.dataset.bsTheme = "dark";
 }
